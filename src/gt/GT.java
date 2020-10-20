f812200f2fbf660abf6bee4fa6899b950578800f
Kimi no Nawa <sqhrsqq@gmail.com>
2020-10-20 15:12:07 +0700

    v.0.0.0

---
+++ src/gt/GT.java
@@ -0,0 +1,14 @@
+
+package gt;
+
+public class GT {
+
+    public static void main(String[] args) {
+        saiHay();
+        }
+    public static void saiHay(){
+        
+      System.out.println("Hi");
+    }
+    
+}
